4b5b37fc827cd1094f8ba0887ffc78f5a4b49681
Michael Chan <mhcchan@ucdavis.edu>
2018-03-25 02:58:49 -0700

    Code cleanup & API response caching
    We kick off version 4.1.0 with a re-evaluation of our 4.0.5 code, re-writing it line by line.
    
    This commit doesn't encompass all of the functionalities in 4.0.5, but does clean up the way we make Boards API calls. I've gone ahead and utilized API response caching to hopefully slightly improve rendering times and decrease load on the Boards API.

--- app_all.4.1.0.js
+++ app_all.4.1.0.js
@@ -1,59 +1,84 @@
-(function($,globals){
+(function($,globals,localStorage){
 	// Define globally scoped object
 	globals.GLOB = {};
+	//var localStorage = window.localStorage;
 	var region = window.location.href.split('.')[1];
 	
 	// Initialize Apollo CORS Bridge
 	var apolloBaseUrl = `https://apollo.${region}.leagueoflegends.com/apollo`;
 	var xhr;
 	$.getScript(`${apolloBaseUrl}/cors/easyXDM.min.js`, ()=> {
-		xhr = new easyXDM.Rpc({
-			remote: `${apolloBaseUrl}/cors/index.html`,
-			swf: `${apolloBaseUrl}/easyXDM.swf`,
-			remoteHelper: `${apolloBaseUrl}/name.html`
-		}, {
-			remote: {
-				request: {}
-			}
-		});
+        xhr = new easyXDM.Rpc({
+            remote: `${apolloBaseUrl}/cors/index.html`,
+            swf: `${apolloBaseUrl}/easyXDM.swf`,
+            remoteHelper: `${apolloBaseUrl}/name.html`
+        }, {
+            remote: {
+                request: {}
+            }
+        });
 	});
 	
 	// Singular function calls
-	pullUserGroups();
+	if(region === "na"){
+		var requestURI = `https://apollo.${region}.leagueoflegends.com/apollo/applications/yrc23zHg`;
+	} else if(region === "oce"){
+		var requestURI = `https://apollo.${region}.leagueoflegends.com/apollo/applications/Ntey9fRZ`;
+	} else if(region === "euw" || region === "eune"){
+		// Filter down languages
+	}
 	
+	// Call Mozilla's Feature-detecting localStorage function
+	if (storageAvailable('localStorage')) {
+		// Is storage available, but groupData not cached?
+		if(!localStorage.getItem('groupData')){
+			// Begin Apollo API call
+			pullUserGroups();
+		} else {
+			// Skip API call, and go straight to parsing
+			var groupData = localStorage.getItem('groupData');
+			applyUserGroups(groupData);
+		}
+	}
+	else {
+		// localStorage is unavailable; make the API call each time.
+		pullUserGroups();
+	}
 	
-	// Pulls the different groups and associated user ID's within groups
 	function pullUserGroups(){
-		// Ensure API call completion before continuing
 		var apiBridgeHandle;
 		clearTimeout(apiBridgeHandle);
 		if(!xhr){
 			apiBridgeHandle = setTimeout(pullUserGroups,0);
 			return;
 		}
-		
-		// Region specific API calls for user groups
-		if(region === "na"){
-			var requestURI = `https://apollo.${region}.leagueoflegends.com/apollo/applications/yrc23zHg`;
-			var myObj = CORSrequest(requestURI);
-		} else if(region === "oce"){
-			var requestURI = `https://apollo.${region}.leagueoflegends.com/apollo/applications/Ntey9fRZ`;
-			var myObj = CORSrequest(requestURI);
-		} else if(region === "euw" || region === "eune"){
-			// Filter down languages
+		CORSrequest(requestURI,applyUserGroups);
+	}
+	
+	// Pulls the different groups and associated user ID's within groups
+	function applyUserGroups(groupData){
+		if (storageAvailable('localStorage')) {
+			localStorage.setItem('groupData',groupData);
 		}
-			
+		// Region specific API calls for user groups
+		var groupData = JSON.parse(localStorage.getItem('groupData'));
+		groupMemberList = Object.keys(groupData.application.metadata.groupsUser);
+		groupMemberData = groupData.application.metadata.groupsUser;
+		console.log(groupMemberList);
+		console.log(groupMemberData);
+		localStorage.setItem('groupMemberList',groupMemberList);
+		localStorage.setItem('groupMemberData',groupMemberData);
 	}
 	
 	// Define CORS request template
-	function CORSrequest(){
+	function CORSrequest(requestURI,callback){
+		console.log(`CORSrequest is ${requestURI}`);
 		xhr.request({
-			url: apiVolunteers,
+			url: requestURI,
 			method: 'GET',
 		}, ({ status, data }) => {
 			// Success!
-			var myObj = JSON.parse(data);
-			return myObj;
+			callback(data);
 		}, ({ status, data }) => {
 			// Error callback //
 			console.log(status);
@@ -61,5 +86,31 @@
 		});
 	}
 	
-}(jQuery,this));
+	// Mozilla's Feature-detecting localStorage function
+	// From: https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+	function storageAvailable(type) {
+		try {
+			var storage = window[type],
+				x = '__storage_test__';
+			storage.setItem(x, x);
+			storage.removeItem(x);
+			return true;
+		}
+		catch(e) {
+			return e instanceof DOMException && (
+				// everything except Firefox
+				e.code === 22 ||
+				// Firefox
+				e.code === 1014 ||
+				// test name field too, because code might not be present
+				// everything except Firefox
+				e.name === 'QuotaExceededError' ||
+				// Firefox
+				e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+				// acknowledge QuotaExceededError only if there's something already stored
+				storage.length !== 0;
+		}
+	}
+	
+}(jQuery,this,this.localStorage));
 
